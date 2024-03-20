765021de61e2bcb85992dc1359a41e9096845313
Ahmad Bin Rashid <ahmadbinrashid92@gmail.com>
2024-03-20 23:15:31 +0500

    Uploaded Files

---
+++ Breakout-Blast/Breakout-Blast/Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace Breakout_Blast
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.ScoreLb = new System.Windows.Forms.Label();
+            this.Player = new System.Windows.Forms.PictureBox();
+            this.Ball = new System.Windows.Forms.PictureBox();
+            this.GameTimer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.Player)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Ball)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ScoreLb
+            // 
+            this.ScoreLb.AutoSize = true;
+            this.ScoreLb.Font = new System.Drawing.Font("Elephant", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ScoreLb.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.ScoreLb.Location = new System.Drawing.Point(697, 9);
+            this.ScoreLb.Name = "ScoreLb";
+            this.ScoreLb.Size = new System.Drawing.Size(91, 25);
+            this.ScoreLb.TabIndex = 0;
+            this.ScoreLb.Text = "Score: 0";
+            // 
+            // Player
+            // 
+            this.Player.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.Player.Location = new System.Drawing.Point(362, 499);
+            this.Player.Name = "Player";
+            this.Player.Size = new System.Drawing.Size(100, 25);
+            this.Player.TabIndex = 1;
+            this.Player.TabStop = false;
+            this.Player.Tag = "Player";
+            // 
+            // Ball
+            // 
+            this.Ball.BackColor = System.Drawing.Color.Gold;
+            this.Ball.Location = new System.Drawing.Point(402, 443);
+            this.Ball.Name = "Ball";
+            this.Ball.Size = new System.Drawing.Size(20, 20);
+            this.Ball.TabIndex = 1;
+            this.Ball.TabStop = false;
+            this.Ball.Tag = "Ball";
+            // 
+            // GameTimer
+            // 
+            this.GameTimer.Tick += new System.EventHandler(this.gameTimerEvent);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(20)))), ((int)(((byte)(20)))));
+            this.ClientSize = new System.Drawing.Size(800, 536);
+            this.Controls.Add(this.Ball);
+            this.Controls.Add(this.Player);
+            this.Controls.Add(this.ScoreLb);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Breakout-Blast";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.keyDownEvent);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.keyUpEvent);
+            ((System.ComponentModel.ISupportInitialize)(this.Player)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Ball)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label ScoreLb;
+        private System.Windows.Forms.PictureBox Player;
+        private System.Windows.Forms.PictureBox Ball;
+        private System.Windows.Forms.Timer GameTimer;
+    }
+}
+
